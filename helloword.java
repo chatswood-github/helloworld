fecd233ad03830cace92fdfeff265e04666766c8
chatswood-github <robin.qd.2011@gmail.com>
2016-03-06 19:24:13 +1100

    Create helloword.java

---
+++ helloword.java
@@ -0,0 +1,11 @@
+public class helloworld
+
+{ public static void main (String [] args)
+
+    {
+            System.out.println("java hello world");
+            System.out.println("java hello worldagain");
+
+    }
+
+}
